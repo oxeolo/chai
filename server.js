9925373e5586a24472ef59183ab4b3e018f1b021
Jay Mohile <jamohile@gmail.com>
2019-09-09 00:00:29 -0400

    Ready for deploy

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-var mysql = require('mysql2/promise');
-require('dotenv').config();
+var mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(`${__dirname}/frontend/build`));
 
-var jwt = require('jwt-simple');
-var bcrypt = require('bcrypt');
+var jwt = require("jwt-simple");
+var bcrypt = require("bcrypt");
 
 app.listen(4000);
 
@@ -20,20 +21,20 @@ app.listen(4000);
 
 MYSQL DATABASE
 
-*/ 
+*/
 
 const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE
-}
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE
+};
 
 let connection;
 
 const connect = async () => {
-    connection = await mysql.createConnection(dbConfig)
-}
+  connection = await mysql.createConnection(dbConfig);
+};
 connect();
 
 // connection.connect(function(err){
@@ -54,7 +55,6 @@ connect();
 //     });
 // });
 
-
 /*
 
 USER API
@@ -62,49 +62,52 @@ USER API
 */
 
 function createUser(req, res, next) {
-    next();
+  next();
 }
 
-app.post('/api/users', async (req, res, next) => {
+app.post("/api/users", async (req, res, next) => {
+  const { email, password } = req.body;
 
-    const {email, password} = req.body;
-
-    const [rows] = await connection.execute(`
+  const [rows] = await connection.execute(
+    `
         SELECT 
             id 
         FROM 
             users 
         WHERE 
             email = ?
-    `, [email])
+    `,
+    [email]
+  );
 
-    if (rows.length > 0) {
-        res.sendStatus(409);
-    }
+  if (rows.length > 0) {
+    res.sendStatus(409);
+  }
 
-    const saltRounds = 10;
+  const saltRounds = 10;
 
-    bcrypt.hash(password, saltRounds, async function(err, hash) {
-        // Store hash in your password DB.
+  bcrypt.hash(password, saltRounds, async function(err, hash) {
+    // Store hash in your password DB.
 
-        const [response] = await connection.execute(`
+    const [response] = await connection.execute(
+      `
             INSERT INTO 
                 users(email, hash) 
             VALUES 
                 (?, ?)
-        `, [email, hash])
+        `,
+      [email, hash]
+    );
 
-        res.sendStatus(201);
-        });
-})
+    res.sendStatus(201);
+  });
+});
 
-const key = 'abcd';
+app.post("/api/users/login", async (req, res, next) => {
+  const { email, password } = req.body;
 
-app.post('/api/users/login', async (req, res, next) => {
-
-    const {email, password} = req.body;
-
-    const [rows] = await connection.execute(`
+  const [rows] = await connection.execute(
+    `
         SELECT 
             id, 
             hash 
@@ -112,34 +115,32 @@ app.post('/api/users/login', async (req, res, next) => {
             users 
         WHERE 
             email = ?
-    `, [email])
+    `,
+    [email]
+  );
 
-    if (rows.length == 0){
-        res.sendStatus(404)
-    }
+  if (rows.length == 0) {
+    res.sendStatus(404);
+  }
 
-    //check password
-    const match = await bcrypt.compare(password, rows[0].hash);
-    
-    if(match) {
+  //check password
+  const match = await bcrypt.compare(password, rows[0].hash);
 
-        //login
-        const db = {
-            "id": rows[0].id
-        };
+  if (match) {
+    //login
+    const db = {
+      id: rows[0].id
+    };
 
-        const token = jwt.encode(db, key);
-    
-        res.json({
-            "token": token
-        });
-    }
-    
-    else{
-        res.sendStatus(404)
-    }
+    const token = jwt.encode(db, process.env.JWT_SECRET);
 
-})
+    res.json({
+      token: token
+    });
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 /*
 
@@ -148,13 +149,13 @@ MIDDLEWARE
 */
 
 function isLoggedIn(req, res, next) {
-    var receivedToken = req.headers.authorization.split("Bearer ")[1]; //probably gonna change but whatever
-    var decodedToken = jwt.decode(receivedToken, key)
-    req.user = decodedToken;
+  var receivedToken = req.headers.authorization.split("Bearer ")[1]; //probably gonna change but whatever
+  var decodedToken = jwt.decode(receivedToken, key);
+  req.user = decodedToken;
 
-    /*assuming we have some code here to make sure their token is legit*/
+  /*assuming we have some code here to make sure their token is legit*/
 
-    next(); //prob under a if statement, else return false? 
+  next(); //prob under a if statement, else return false?
 }
 
 /*
@@ -163,13 +164,11 @@ BOOKS API
 
 */
 
+app.get("/api/books", isLoggedIn, async (req, res, next) => {
+  /* gets user's books from DB. */
 
-
-app.get('/api/books', isLoggedIn, async (req, res, next) => {
-
-    /* gets user's books from DB. */
-
-    const [rows] = await connection.execute(`
+  const [rows] = await connection.execute(
+    `
         SELECT 
             id, 
             color, 
@@ -179,29 +178,32 @@ app.get('/api/books', isLoggedIn, async (req, res, next) => {
             books 
         WHERE 
             userID = ?
-    `, [req.user.id])
+    `,
+    [req.user.id]
+  );
 
-    res.json(rows)
-})
+  res.json(rows);
+});
 
-app.post('/api/books', isLoggedIn, async (req, res, next) => {
+app.post("/api/books", isLoggedIn, async (req, res, next) => {
+  const { color, name, content = "" } = req.body;
 
-    const {color, name, content=""} = req.body;
-
-    const [response] = await connection.execute(`
+  const [response] = await connection.execute(
+    `
         INSERT INTO 
             books(userID, color, name, content) 
         VALUES 
             (?, ?, ?, ?)
-    `, [req.user.id, color, name, content])
+    `,
+    [req.user.id, color, name, content]
+  );
 
-    res.sendStatus(201)
-    
+  res.sendStatus(201);
 });
 
-app.get('/api/books/:id', isLoggedIn, async (req, res) => {
-
-    const [rows] = await connection.execute(`
+app.get("/api/books/:id", isLoggedIn, async (req, res) => {
+  const [rows] = await connection.execute(
+    `
         SELECT 
             id, 
             userID, 
@@ -213,16 +215,18 @@ app.get('/api/books/:id', isLoggedIn, async (req, res) => {
         WHERE 
             id = ? AND 
             userID = ?
-    `, [req.params.id, req.user.id])
+    `,
+    [req.params.id, req.user.id]
+  );
 
-    res.json(rows[0])
-
+  res.json(rows[0]);
 });
 
-app.put('/api/books/:id', isLoggedIn, async (req, res, next) => {
-    const {content="", name, color} = req.body;
+app.put("/api/books/:id", isLoggedIn, async (req, res, next) => {
+  const { content = "", name, color } = req.body;
 
-    const [response] = await connection.execute(`
+  const [response] = await connection.execute(
+    `
         UPDATE 
             books 
         SET 
@@ -232,10 +236,13 @@ app.put('/api/books/:id', isLoggedIn, async (req, res, next) => {
         WHERE 
             id = ? AND 
             userID = ?
-    `, [content, name, color, req.params.id, req.user.id])
+    `,
+    [content, name, color, req.params.id, req.user.id]
+  );
 
-    res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
-
-
+app.get("/*", (req, res) => {
+  res.sendFile(`${__dirname}/frontend/build/index.html`);
+});
